b1edd24456fa7639a2ab48a7c4c6d330cb6f5687
Kasmi Thapa <kasmithapa00@gmail.com>
2024-11-19 19:06:51 +0545

    finalchange

--- dart_application_1/lib/odd_even.dart
+++ dart_application_1/lib/odd_even.dart
@@ -1,29 +1,12 @@
-//checking the number is even oor odd
-import 'dart:io';
-
-void main() {
-  print("Enter a number:");
-  String? input = stdin.readLineSync();
-  
-  // Check if the input is not null and can be parsed to an integer
-  if (input != null) {
-    int? number = int.tryParse(input);
-    
-    if (number != null) {
-      print(checkEvenOdd(number));
-    } else {
-      print("Invalid input. Please enter a valid integer.");
-    }
-  } else {
-    print("No input received.");
-  }
+void main(){
+  print(checkOddEven(5));
+  print(checkOddEven(8));
 }
 
-String checkEvenOdd(int num) {
-  if (num % 2 == 0) {
-    return "$num is Even";
+String checkOddEven(int number) {
+  if (number % 2 == 0) {
+    return "even";
   } else {
-    return "$num is Odd";
+    return "odd";
   }
 }
-
