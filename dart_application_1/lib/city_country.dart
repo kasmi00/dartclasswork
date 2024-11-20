4c68e2f387991f5461ef74729338196a49d90462
Kasmi Thapa <kasmithapa00@gmail.com>
2024-11-20 09:54:49 +0545

    classtask

--- dart_application_1/lib/city_country.dart
+++ dart_application_1/lib/city_country.dart
@@ -4,10 +4,16 @@ void main(){
   cityCountry['London']='UK';
   cityCountry['Paris']='France';
   cityCountry['Berlin']='Germany';
-  
-  cityCountry.forEach((city,Country){
-    print('$city is in $Country');
-  });
+  cityCountry['Tokyo'] = 'Japan'; 
+String searchValue ="London";
+print("$searchValue is in ${cityCountry[searchValue]}");
+
+}
+
+
+  // cityCountry.forEach((city,Country){
+  //   print('$city is in $Country');
+  // });
     
   
-  }
+  
