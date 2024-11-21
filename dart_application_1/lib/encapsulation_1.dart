791a3983296915913e42897f5a9fd6a9e8b29065
Kasmi Thapa <kasmithapa00@gmail.com>
2024-11-21 20:33:11 +0545

    encapsulation

---
+++ dart_application_1/lib/encapsulation_1.dart
@@ -0,0 +1,19 @@
+//ENCAPSULATION
+class Rectangle {
+  int length;
+  int breadth;
+
+  Rectangle(this.length, this.breadth);
+
+  void getPerimeter() {
+    int perimeter = 2 * (length + breadth);
+    print("Perimeter: $perimeter");
+  }
+}
+
+void main() {
+  var rectangle = Rectangle(5, 6);
+  rectangle.getPerimeter();
+}
+
+
